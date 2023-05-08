b5414d1bc746d3ea2f5c57416fd76fd50d7b6336
folathecoder <folathecoder@gmail.com>
2023-05-08 02:53:05 +0100

    Removed unused code

--- Controllers/GraphConstructor.cs
+++ Controllers/GraphConstructor.cs
@@ -315,31 +315,5 @@ namespace tflzone1.Controllers
             // Waterloo & City Station Vertices
             graph.AddEdge(new Vertex("waterloo & city: waterloo"), new Vertex("waterloo & city: bank"), 33);
         }
-
-
-
-
-
-
-        // public static void ConstructGraph()
-        // {
-        //     graph = new Graph();
-        //     graph.AddVertex("northern: a");
-        //     graph.AddVertex("bakerloo: b");
-        //     graph.AddVertex("circle: c");
-        //     graph.AddVertex("district: d");
-        //     graph.AddVertex("hammersmith: e");
-        //     graph.AddVertex("picadilly: f");
-
-        //     graph.AddEdge(new Vertex("northern: a"), new Vertex("bakerloo: b"), 7);
-        //     graph.AddEdge(new Vertex("northern: a"), new Vertex("circle: c"), 9);
-        //     graph.AddEdge(new Vertex("northern: a"), new Vertex("picadilly: f"), 14);
-        //     graph.AddEdge(new Vertex("bakerloo: b"), new Vertex("circle: c"), 10);
-        //     graph.AddEdge(new Vertex("bakerloo: b"), new Vertex("district: d"), 15);
-        //     graph.AddEdge(new Vertex("circle: c"), new Vertex("district: d"), 11);
-        //     graph.AddEdge(new Vertex("circle: c"), new Vertex("picadilly: f"), 2);
-        //     graph.AddEdge(new Vertex("district: d"), new Vertex("hammersmith: e"), 6);
-        //     graph.AddEdge(new Vertex("hammersmith: e"), new Vertex("picadilly: f"), 9);
-        // }
     }
 }
